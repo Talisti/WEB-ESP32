8db05fb6d45ba82f88e2ca2839884915c2ec44f6
Talisti <calisti.theo25@yahoo.com>
2024-10-01 11:26:57 +0200

    14

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,42 @@
-#include <WiFi.h>
-#include <WebServer.h>
+#include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
 
-// Potentiomètre relié à GPIO 34 (Analog ADC1_CH6)
-const int potPin = 34;
+//const char *ssid = "Nom du réseau";
+//const char *password = "Mot de passe";
 
 const char* ssid = "iPhone de Theo";
 const char* password = "lamoine1972";
-WebServer server(80);
-const int led = 2;
-bool etatLed = 0;
-char texteEtatLed[2][10] = {"ÉTEINTE!","ALLUMÉE!"};
-void handleRoot()
-{
-String page = "<!DOCTYPE html>";
-page += "<html lang='fr'>";
-page += "<head>";
-page += " <title>Serveur ESP32</title>";
 
-page += " <meta http-equiv='refresh' content='60' name='viewport' content='width=device-width, initial-scale=1' charset='UTF-8' />";
-
-page += " <link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>";
-page += "</head>";
-page += "<body>";
-page += " <div class='w3-card w3-blue w3-padding-small w3-jumbo w3-center'>";
-page += " <p>Valeur Potentiomètre : "; page += String(analogRead(potPin)); + "</p>";
-page += " </div>";
-page += " <div class='w3-bar'>";
-page += " <a href='/on' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>ON</a>";
-page += " <a href='/off' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>OFF</a>";
-page += " </div>";
-page += " <div class='w3-center w3-padding-16'>";
-page += " <p>Serveur hébergé sur un ESP32</p>";
-page += " <i>Projet SNBot</i>";
-page += " </div>";
-page += "</body>";
-page += "</html>";
-server.setContentLength(page.length());
-server.send(200, "text/html", page);
-}
-void handleOn()
-{
-etatLed = 1;
-digitalWrite(led, HIGH);
-server.sendHeader("Location","/");
-server.send(303);
-}
-void handleOff()
-{
-etatLed = 0;
-digitalWrite(led, LOW);
-server.sendHeader("Location","/");
-server.send(303);
-}
-void handleNotFound()
-{
-server.send(404, "text/plain", "404: Not found");
-}
+const int led = 2; // Led intégrée à l'ESP32
+AsyncWebServer server(80);
 void setup()
 {
+
 Serial.begin(9600);
-delay(1000);
-Serial.println("\n");
 pinMode(led, OUTPUT);
 digitalWrite(led, LOW);
-WiFi.persistent(false);
-WiFi.begin(ssid, password);
+//---------------------------SPIFFS-------------------
+if(!SPIFFS.begin()) /* Démarrage du gestionnaire de fichiers SPIFFS */
+{
+Serial.println("Erreur SPIFFS...");
+return;
+}
+/* Détection des fichiers présents sur l'Esp32 */
+File root = SPIFFS.open("/"); /* Ouverture de la racine */
+File file = root.openNextFile(); /* Ouverture du 1er fichier */
+while(file) /* Boucle de test de présence des fichiers - Si plus de fichier
+s la boucle s'arrête*/
+{
+Serial.print("File: ");
+Serial.println(file.name());
+file.close();
+file = root.openNextFile(); /* Lecture du fichier suivant */
+}
+//-----------------------WIFI-----------------------------
+WiFi.begin(ssid, password); /* Connexion au réseau Wifi */
 Serial.print("Tentative de connexion...");
-while (WiFi.status() != WL_CONNECTED)
+while(WiFi.status() != WL_CONNECTED)
 {
 Serial.print(".");
 delay(100);
@@ -75,14 +45,31 @@ Serial.println("\n");
 Serial.println("Connexion etablie!");
 Serial.print("Adresse IP: ");
 Serial.println(WiFi.localIP());
-server.on("/", handleRoot);
-server.on("/on", handleOn);
-server.on("/off", handleOff);
-server.onNotFound(handleNotFound);
-server.begin();
-Serial.println("Serveur web actif!");
-}
-void loop()
+//--------------------------SERVEUR--------------------------
+/* Lorsque le serveur est actif , la page index.html est chargée */
+server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request){
+request->send(SPIFFS, "/w3.css", "text/css");
+});
+server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
 {
-server.handleClient();
+request->send(SPIFFS, "/index.html", "text/html");
+});
+/* Lorsque l'on clique sur ON, on allume la led */
+server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
+{
+digitalWrite(led, HIGH);
+request->send(SPIFFS, "/index.html", "text/html");
+});
+/* Lorsque l'on clique sur OFF, on éteint la led */
+server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
+{
+digitalWrite(led, LOW);
+request->send(SPIFFS, "/index.html", "text/html");
+});
+/* On affiche que le serveur est actif */
+server.begin();
+Serial.println("Serveur actif!");
+}
+void loop() /* La loop est vide */
+{
 }
